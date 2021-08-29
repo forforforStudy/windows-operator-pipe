01decfac886c8b36c5a3bef4ccf84c699df64629
forforforStudy <411162428@qq.com>
2021-08-29 17:52:42 +0800

    feat: 支持正则跳过某些匹配的文件或者目录

--- wop_modules/zip_processor/__init__.py
+++ wop_modules/zip_processor/__init__.py
@@ -1,4 +1,5 @@
 import os
+import re
 
 from zipfile import ZipFile, ZIP_DEFLATED
 
@@ -6,11 +7,27 @@ from wop_modules.zip_processor.zip_processor_config import ZipProcessorConfig
 
 
 class ZipProcessor(object):
+    """
+    执行zip压缩运行能力
+    """
     @staticmethod
     def execute(config: ZipProcessorConfig):
         zip_target_path = config.zip_target_path
         directory_path = config.directory_path
+        ignore_regexps = config.ignore_regexps
 
+        '''
+        传入的 ignore_regexps 进行正则的忽略能力
+        '''
+        def ignore_regexps_re_tester(text: str):
+            if ignore_regexps and len(ignore_regexps) > 0:
+                for regexp in ignore_regexps:
+                    if re.search(regexp, text, re.M) is None:
+                        return False
+            else:
+                return True
+
+        # zip压缩文件对象
         zipfile = ZipFile(
             zip_target_path,
             'w',
@@ -21,12 +38,11 @@ class ZipProcessor(object):
             for filename in filenames:
                 zip_filename: str = os.path.join(dirpath, filename)
 
-                print(zip_filename, dirpath, filename)
-
-                zipfile.write(
-                    zip_filename,
-                    arcname=zip_filename.replace(directory_path, '')
-                )
+                if ignore_regexps_re_tester(zip_filename):
+                    zipfile.write(
+                        zip_filename,
+                        arcname=zip_filename.replace(directory_path, '')
+                    )
 
         zipfile.close()
 
--- wop_modules/zip_processor/zip_processor_config.py
+++ wop_modules/zip_processor/zip_processor_config.py
@@ -1,7 +1,16 @@
+from typing import List
+
+
 class ZipProcessorConfig(object):
 
-    def __init__(self, directory_path: str, zip_target_path: str):
+    def __init__(self,
+                 directory_path: str,
+                 zip_target_path: str,
+                 ignore_regexps: List[str] = []
+                 ):
         # 需要压缩的目录
         self.directory_path = directory_path
         # 生成的zip压缩文件的目标地址
         self.zip_target_path = zip_target_path
+        # 跳过的文件正则数组
+        self.ignore_regexps = ignore_regexps
