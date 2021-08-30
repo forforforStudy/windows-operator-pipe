21836f6012d5e9b1a3f08513ce1a998c173e2023
forforforStudy <411162428@qq.com>
2021-08-30 22:32:21 +0800

    feat: 拷贝文件操作逻辑

--- wop_modules/copyacut_processor/copyacut_processor_config.py
+++ wop_modules/copyacut_processor/copyacut_processor_config.py
@@ -1,14 +1,29 @@
+from typing import List
+
+import os
+import shutil
 import wop_modules.copyacut_processor.copyacut_processor_action as cpa
 
+
 class CopyacutProcessorConfig(object):
-
-    def __init__(self, action: cpa.CopyacutProcessorAction, from_path: str, to_path: str):
+    def __init__(
+            self,
+            action: cpa.CopyacutProcessorAction,
+            from_path: str,
+            to_path: str,
+            skip_regexps: List[str] = None
+    ):
         self.action = action
-
         self.from_path = from_path
         self.to_path = to_path
+        self.skip_regexps = [] if skip_regexps is None else skip_regexps
 
     def execute(self):
         from_path = self.from_path
         to_path = self.to_path
+        action = self.action
 
+        if action is cpa.CopyacutProcessorAction.COPY:
+            shutil.copytree(from_path, to_path)
+        elif action is cpa.CopyacutProcessorAction.CUT:
+            os.rename(from_path, to_path)
