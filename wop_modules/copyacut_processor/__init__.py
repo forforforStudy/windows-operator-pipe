97517985b7148c1d4b38ebabddb1b299971a6611
forforforStudy <411162428@qq.com>
2021-08-30 22:37:26 +0800

    feat: 修正文件夹复制和剪切的执行函数

--- wop_modules/copyacut_processor/__init__.py
+++ wop_modules/copyacut_processor/__init__.py
@@ -1,9 +1,25 @@
 """
 复制或者剪切的处理器
 """
+import os
+import shutil
+
+from wop_modules.copyacut_processor.copyacut_processor_action import CopyacutProcessorAction
+from wop_modules.copyacut_processor.copyacut_processor_config import CopyacutProcessorConfig
 
 
 class CopyacutProcessor(object):
+    def __init__(self, config: CopyacutProcessorConfig):
+        self.config = config
 
     def execute_copy_and_cut(self):
-        pass
+        config = self.config
+
+        from_path = config.from_path
+        to_path = config.to_path
+        action = config.action
+
+        if action is CopyacutProcessorAction.COPY:
+            shutil.copytree(from_path, to_path)
+        elif action is CopyacutProcessorAction.CUT:
+            os.rename(from_path, to_path)
--- wop_modules/copyacut_processor/copyacut_processor_config.py
+++ wop_modules/copyacut_processor/copyacut_processor_config.py
@@ -1,7 +1,5 @@
 from typing import List
 
-import os
-import shutil
 import wop_modules.copyacut_processor.copyacut_processor_action as cpa
 
 
@@ -17,13 +15,3 @@ class CopyacutProcessorConfig(object):
         self.from_path = from_path
         self.to_path = to_path
         self.skip_regexps = [] if skip_regexps is None else skip_regexps
-
-    def execute(self):
-        from_path = self.from_path
-        to_path = self.to_path
-        action = self.action
-
-        if action is cpa.CopyacutProcessorAction.COPY:
-            shutil.copytree(from_path, to_path)
-        elif action is cpa.CopyacutProcessorAction.CUT:
-            os.rename(from_path, to_path)
