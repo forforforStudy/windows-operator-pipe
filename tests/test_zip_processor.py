560f51887d3f5c188362348bd7049717e84350c3
林楠 <282712@nd.com>
2021-08-31 11:00:55 +0800

    test: 优化zip压缩执行单元测试

--- tests/test_zip_processor.py
+++ tests/test_zip_processor.py
@@ -1,3 +1,5 @@
+import os
+import shutil
 import time
 import unittest
 import os.path as path
@@ -7,19 +9,30 @@ import wop_modules.zip_processor.zip_processor_config as zc
 
 
 class ZipProcessorTester(unittest.TestCase):
+    cur_dir = path.dirname(path.realpath(__file__))
+
+    zip_resource_temp_filepath = path.join(cur_dir, 'fixtures', 'zip_processor_zips')
+
+    @classmethod
+    def setUpClass(cls) -> None:
+        # 不存在目录时，创建目录
+        zip_resource_temp_filepath = ZipProcessorTester.zip_resource_temp_filepath
+        if not path.exists(zip_resource_temp_filepath):
+            os.mkdir(zip_resource_temp_filepath)
+
+        # 清空 zip_resource_temp_filepath 目录下的文件
+        for zip_file in os.listdir(zip_resource_temp_filepath):
+            os.remove(path.join(zip_resource_temp_filepath, zip_file))
 
     def setUp(self) -> None:
-        cur_dir = path.dirname(path.realpath(__file__))
         directory_path = path.join(
-            cur_dir,
+            ZipProcessorTester.cur_dir,
             'fixtures',
             'zip_processor_resources'
         )
 
         zip_target_path = path.join(
-            cur_dir,
-            'fixtures',
-            'zip_processor_zips',
+            ZipProcessorTester.zip_resource_temp_filepath,
             'test-file-{}.zip'.format(time.time())
         )
 
