c1b75a3e737db5c0df2f6d6fb65a116e803dbe83
林楠 <411162428@qq.com>
2021-09-01 15:55:52 +0800

    feat: 任务队列执行上下文路径调整

--- wop_modules/pipe_factory/__init__.py
+++ wop_modules/pipe_factory/__init__.py
@@ -1,4 +1,8 @@
+import os
+
 import yaml
+import os.path as path
+import pathlib
 
 from typing import List, TypedDict, Callable
 
@@ -20,9 +24,24 @@ class ConfigParserReturn(TypedDict):
 
 
 class ConfigParser(object):
-    def __init__(self, path: str):
-        yaml_file = open(path)
+    def __init__(self, config_file_path: str):
+        yaml_file = open(config_file_path)
+
         self.yaml_data = yaml.load(yaml_file, Loader=yaml.FullLoader)
+
+        yaml_data_root_value = self.yaml_data.get('root')
+        cur_cwd = os.getcwd()
+
+        """
+        执行上下文的根路径
+        """
+        if yaml_data_root_value is None:
+            self.root = cur_cwd
+        else:
+            if path.isabs(yaml_data_root_value):
+                self.root = yaml_data_root_value
+            else:
+                self.root = path.join(cur_cwd, yaml_data_root_value)
 
     def parser(self) -> ConfigParserReturn:
         yaml_data = self.yaml_data
@@ -31,33 +50,43 @@ class ConfigParser(object):
         tasks_processor: List[Callable] = []
 
         for task in [t for t in tasks if t['action'] in [item.value for item in ConfigActions]]:
-            tasks_processor.append(ConfigParser.map_task_to_processor(task))
+            tasks_processor.append(self.map_task_to_processor(task))
 
         return {'run': lambda: [processor() for processor in tasks_processor]}
 
-    @staticmethod
-    def map_task_to_processor(task: TaskShaped) -> Callable:
+    def handle_os_path(self, any_path: str):
+        if path.isabs(any_path):
+            return any_path
+        else:
+            return str(pathlib.WindowsPath(self.root).joinpath(any_path))
+
+    def map_task_to_processor(self, task: TaskShaped) -> Callable:
         action = task['action']
 
         # shell命令行的处理
         if action == ConfigActions.SHELL.value:
             shell_config = task['shell_config']
-            return lambda: ShellProcessor(shell_config['cwd']).execute(shell_config['cmd'])
+            return lambda: ShellProcessor(self.handle_os_path(shell_config['cwd'])).execute(shell_config['cmd'])
+
         # copy和cut的处理
         elif action == ConfigActions.COPY_AND_CUT.value:
             copy_and_cut_config = task['copyacut_config']
+            copyacut_action = copy_and_cut_config['action']
+
             return lambda: CopyacutProcessor(
                 CopyacutProcessorConfig(
-                    action=CopyacutProcessorAction[copy_and_cut_config['action']],
-                    from_path=copy_and_cut_config['fro'],
-                    to_path=copy_and_cut_config['to'],
-                    override=copy_and_cut_config['override']
+                    action=CopyacutProcessorAction(copyacut_action),
+                    from_path=self.handle_os_path(copy_and_cut_config['fro']),
+                    to_path=self.handle_os_path(copy_and_cut_config['to']),
+                    override=copy_and_cut_config['override'] == 'true'
                 )
             ).execute_copy_and_cut()
+
         # zip处理
         elif action == ConfigActions.ZIP.value:
             zip_config = task['zip_config']
             return lambda: ZipProcessor().execute(
-                ZipProcessorConfig(directory_path=zip_config['fro'], zip_target_path=zip_config['to'],
+                ZipProcessorConfig(directory_path=self.handle_os_path(zip_config['fro']),
+                                   zip_target_path=self.handle_os_path(zip_config['to']),
                                    ignore_regexps=zip_config['ignore'])
             )
--- wop_modules/pipe_factory/config_task_const.py
+++ wop_modules/pipe_factory/config_task_const.py
@@ -1,5 +1,5 @@
 from enum import Enum
-from typing import List, TypedDict, Union
+from typing import List, TypedDict
 
 """
 yaml_data 的格式为:
