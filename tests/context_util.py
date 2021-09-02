01c44ef1d40b0f7bcd48fd2cbaa966b486d66c86
林楠 <411162428@qq.com>
2021-09-02 14:09:57 +0800

    test: pipe-f ut完善

--- tests/context_util.py
+++ tests/context_util.py
@@ -1,5 +1,13 @@
+import os
 import os.path as path
 
 cur_dir = path.dirname(path.realpath(__file__))
 
 fixtures_dir = path.join(cur_dir, 'fixtures')
+
+
+def clean_path(target_path: str) -> None:
+    if path.isdir(target_path):
+        for real_file_path in [path.join(target_path, file_name) for file_name in os.listdir(target_path)]:
+            if path.isfile(real_file_path):
+                os.remove(real_file_path)
--- tests/test_pipe_factory.py
+++ tests/test_pipe_factory.py
@@ -1,8 +1,8 @@
 import unittest
 import os.path as path
 
-from tests.context_util import fixtures_dir
-from wop_modules.pipe_factory import ConfigParser, ConfigParserReturn
+from tests.context_util import fixtures_dir, clean_path
+from wop_modules.pipe_factory import ConfigParser, ConfigParserReturn, global_context
 
 
 class PipeFactoryTester(unittest.TestCase):
@@ -12,6 +12,14 @@ class PipeFactoryTester(unittest.TestCase):
             'pipe_config.yml'
         )
     )
+
+    @classmethod
+    def setUpClass(cls) -> None:
+        # 清理测试数据
+        clean_path(path.join(fixtures_dir, 'pipe_factory_resources/copy_results'))
+        clean_path(path.join(fixtures_dir, 'pipe_factory_resources/results'))
+
+        clean_path(path.join(fixtures_dir, 'pipe_factory_resources'))
 
     def test_pipe_config_yaml_run(self):
         self.assertTrue(type(PipeFactoryTester.config_parser.yaml_data) is dict)
