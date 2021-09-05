a4209ee53f7734f768717770b2c98b332096e873
forforforStudy <411162428@qq.com>
2021-09-05 16:23:02 +0800

    feat: 入口文件命令行参数配置

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
 import os
 import sys
 import pathlib
+import argparse
 
 from wop_modules.pipe_factory import ConfigParser, ConfigParserReturn
 
 default_config_filename = 'pipe_config.yml'
 
 if __name__ == '__main__':
-    config_path = sys.argv[1]
+    '''
+    解析参数
+    '''
+    arg_parser = argparse.ArgumentParser()
+    arg_parser.add_argument('-c', '--config', help='输入配置文件的文件路径')
+    arg_namespace = arg_parser.parse_args()
+
+    config_path = arg_namespace.config
 
     if config_path is None:
         # 默认路径值
@@ -16,7 +24,7 @@ if __name__ == '__main__':
     if not pathlib.WindowsPath(config_path).exists():
         raise RuntimeError('配置文件路径有误，请检查: {}'.format(config_path))
     else:
-        config_parser = ConfigParser
+        config_parser = ConfigParser(config_file_path=config_path)
 
         parsed: ConfigParserReturn = config_parser.parser()
         parsed['run']()
